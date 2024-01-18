c65aceccc7b0ac9b202afba392a670ec32baba16
paolaromanvaldes <paolaromanvaldes@gmail.com>
2024-01-18 12:41:15 -0800

    final

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "88d7878abbc650729750feb638ea468a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "1f399452ec30453c8589c5bd709d7cf7"
